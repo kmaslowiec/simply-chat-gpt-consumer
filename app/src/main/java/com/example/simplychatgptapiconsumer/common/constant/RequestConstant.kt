deedbd7ef949eeb990d0543f749719d3d543106a
Konrad <kmaslowiec@gmail.com>
2024-02-15 14:40:31 +0100

    Input field UI

--- app/src/main/java/com/example/simplychatgptapiconsumer/common/constant/RequestConstant.kt
+++ app/src/main/java/com/example/simplychatgptapiconsumer/common/constant/RequestConstant.kt
@@ -1,4 +1,4 @@
-package com.example.simplychatgptapiconsumer.common.constants
+package com.example.simplychatgptapiconsumer.common.constant
 
 const val SYSTEM_INSTRUCTION = "TRIVIAAPP"
 const val CHAT_GPT_MODEL_ID = "ft:gpt-3.5-turbo-1106:personal::8kvEh76H"
--- app/src/main/java/com/example/simplychatgptapiconsumer/main/MainScreen.kt
+++ app/src/main/java/com/example/simplychatgptapiconsumer/main/MainScreen.kt
@@ -1,35 +1,78 @@
 package com.example.simplychatgptapiconsumer.main
 
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import com.example.simplychatgptapiconsumer.common.model.ChatAnswerState.Error
-import com.example.simplychatgptapiconsumer.common.model.ChatAnswerState.Loading
-import com.example.simplychatgptapiconsumer.common.model.ChatAnswerState.Success
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.simplychatgptapiconsumer.R
+import com.example.simplychatgptapiconsumer.common.model.ChatAnswerState
+
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-
-    when (val state = viewModel.questionsState.collectAsState().value) {
-        is Loading -> {}
-        is Success -> {
-            Greeting(state.questions.content)
+    var textState by remember { mutableStateOf("") }
+    var errorState by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.fillMaxSize()) {
+        TriviaSubject(
+            textState, errorState = errorState,
+            onValueChange = {
+                textState = it
+                errorState = textState.length > 30
+            },
+        )
+        when (val state = viewModel.questionsState.collectAsState().value) {
+            is ChatAnswerState.Loading -> {} //TODO add loader
+            is ChatAnswerState.Success -> {} //TODO add chat result
+            is ChatAnswerState.Error -> {} //TODO handle error
         }
-
-        is Error -> {}
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val scrollState = rememberScrollState(0)
-    Text(
-        text = "ChatGtp says: $name",
-        modifier = modifier.verticalScroll(state = scrollState, enabled = true)
-            .testTag("MainTextField")
+fun TriviaSubject(
+    value: String,
+    errorState: Boolean,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(R.string.mainScreenTriviaSubjectLabelText)) },
+        modifier = Modifier
+            .testTag(stringResource(R.string.mainScreenTriviaSubjectTestTag))
+            .wrapContentSize()
+            .fillMaxWidth()
+            .padding(16.dp),
+        singleLine = true,
+        isError = errorState,
+        supportingText = {
+            if (errorState) {
+                Text(
+                    stringResource(R.string.mainScreenTriviaSubjectSupportingText),
+                    color = Color.Red
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions().copy(
+            imeAction = ImeAction.Send,
+            keyboardType = KeyboardType.Text
+        ),
     )
 }
